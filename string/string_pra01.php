89c75263cd298dc0e7bfeb33014510b001ac222f
mackliu <mackliu@wda.gov.tw>
2022-10-17 11:04:01 +0800

    新增字串整合練習題目

--- string/string_pra01.php
+++ string/string_pra01.php
@@ -20,3 +20,20 @@ echo "<hr>";
 echo mb_substr("The reason why a great man is great is that he resolves to be a great man",0,10)."...";
 
 ?>
+<h1>字串函式整合應用</h1>
+<?php
+$target='程式設計';
+$str="學會PHP網頁程式設計，薪水會加倍，工作會好找";
+$sub="<span style='font-size: 24px;color:red'>".$target."</span>";
+$result=str_replace($target,$sub,$str);
+echo $str."<br>";
+echo $result."<br>";
+
+//print_r(explode($target,$str));
+echo join($sub,explode($target,$str))
+?>
+
+
+<!-- 學會PHP網頁<span style="font-size:24px;color:red">程式設計</span>，薪水會加倍，工作會好找 -->
+
+
